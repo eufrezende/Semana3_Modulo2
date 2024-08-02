ea116776922ed29439d697e367bff3adcde2e368
Felipe Rezende <felipe.rrezend@gmail.com>
2024-08-02 14:14:09 -0300

    Entendendo leitura de argumentos.

--- 2_FUNDAMENTOS/4_ler_argumentos/index.js
+++ 2_FUNDAMENTOS/4_ler_argumentos/index.js
@@ -1,11 +1,17 @@
 // nome=Matheus
 
-console.log(process.argv);
+console.log(process.argv); // mostra o executável, o arquivo e o argumento
 
-const args = process.argv.slice(2);
+const args = process.argv.slice(2); //o slice usado apra resgatar o segundo índice (que no caso seria apenas o argumento do process.argv)
 
 console.log(args);
 
-const nome = args[0].split("=")[1];
+const nome = args[0].split("=")[1]; //o split divide a string quando vê um =, fazendo com que ele retorne apenas o valor do nome
 
 console.log(nome);
+
+const idade = args[1].split("=")[1];
+
+console.log(idade);
+
+console.log(`Onome dele é ${nome} e ele tem ${idade} anos.`)
