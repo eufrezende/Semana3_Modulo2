6c126f2482aa2b537c25a9c3d1aaedd965cbef30
Felipe Rezende <felipe.rrezend@gmail.com>
2024-08-03 19:02:55 -0300

    Finalizando seção 6

--- 6_EXPRESS/11_tarefa_3/projects/index.js
+++ 6_EXPRESS/11_tarefa_3/projects/index.js
@@ -5,12 +5,12 @@ const path = require('path')
 
 const basePath = path.join(__dirname, '../templates')
 
-router.get('/', (req, res) => {
-  res.sendFile(`${basePath}/projects.html`)
+router.get('/pagina1', (req, res) => {
+  res.sendFile(`${basePath}/pagina1.html`)
 })
 
 router.get('/:id', (req, res) => {
-  res.sendFile(`${basePath}/project.html`)
+  res.sendFile(`${basePath}/pagina2.html`)
 })
 
 module.exports = router
